6a6ad3aa56918d40c22190ea10249ee33b6a929a
Mannan-Goyal <mannnang6@gmail.com>
2021-11-11 10:16:39 +0530

    fix: no success msg on deletion of non-existent reminder

--- bot.py
+++ bot.py
@@ -14,8 +14,8 @@ import db
 import getBalance
 
 load_dotenv()
-TOKEN = os.getenv('TOKEN')
-# TOKEN = os.getenv('DEV_TOKEN')
+# TOKEN = os.getenv('TOKEN')
+TOKEN = os.getenv('DEV_TOKEN')
 
 bot = commands.Bot(command_prefix="+")
 bot.remove_command('help')
@@ -320,9 +320,13 @@ async def delreminder(ctx,*,args=None):
                         return
                     else:
                         res=db.delReminder(username,region,discord_id,weapon)
+                        print(res)
                         if res:
                             embed = thumbnailEmbed("Reminder Deleted!","The reminder has been deleted successfully!","https://emoji.gg/assets/emoji/confetti.gif")
                             await ctx.channel.send(embed=embed)
+                        else:
+                            error_embed = smallEmbed("Error","There was a problem in deleting that reminder!")
+                            await ctx.channel.send(embed=error_embed)
                 except:
                     embed=exceptionEmbed()
                     await ctx.channel.send(embed=embed)
